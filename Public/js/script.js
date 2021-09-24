fc700ea4b436ad2f71f6e1b64f210d6b77c2141f
Randerson27 <69997038+Randerson27@users.noreply.github.com>
2021-09-24 15:18:16 -0300

    validando o formulario

--- Public/js/script.js
+++ Public/js/script.js
@@ -1,12 +1,22 @@
 "use strict"
 
+//EVENTS
 $("#btnNew").click(function(){
   openModalCreate(true)
 
 });
 
+$("#frmGame").submit(function(e){
+
+  sendForm();
+  e.preventDefault();
+});
+
 function openModalCreate(reset = true){
   $('#modalNewGame').modal('show');
+
+  if(reset)
+  resetForm();
 }
 
 function openModalViewGame  (id){
@@ -19,3 +29,56 @@ function deleteGame(id){
 
   console.log(id);
 }
+
+//SEND
+
+function sendForm(){
+  var obj = {
+    id : $("txtId").val(),
+    title : $("txtTitle").val(),
+    description : $("txtDescription").val(),
+    videoid : $("txtVideoid").val()
+  };
+
+  var result = validate(obj);
+  if(result != ""){
+    $("#dvAlert").text(result);
+    return;
+  }
+
+  if(obj.id == 0){
+    console.log("Cadastrar");
+  }else{
+    console.log("Editar");
+  }
+  
+  console.log(obj);
+}
+
+function validate(obj){
+
+  if(obj.id < 0){
+    return "ID invalido"
+  }
+  if(obj.title.length < 4 || obj.title.length > 100){
+    return "Titulo invalido";
+  }
+
+  if(obj.description.length < 10 || obj.description.length > 250){
+    return "Descricao Invalida";
+  }
+
+  if(obj.videoid == "" || obj.description.length > 15){
+    return "Video ID inválido";
+  }
+
+  return "";
+}
+
+function resetForm(){
+    $("#txtId").val("0");
+    $("#txtTitle").val("");
+    $("#txtDescription").val("");
+    $("#txtVideoid").val("");
+    $("#dvAlert").html("&nbsp;");
+  }
